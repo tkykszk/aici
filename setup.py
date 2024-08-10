f1685567802ebce9755dcd68deed606c96ef1409
suzuki takayuki <username@users.noreply.github.com>
2024-08-10 23:06:34 +0900

    0.0.2 release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="aici",
-    version="0.0.1",
+    version="0.0.2",
     packages=find_packages(),
     description="A command line interface for ChatGPT",
     long_description=open('README.md').read(),    
